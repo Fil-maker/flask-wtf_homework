02010533fbbd2cd211e85341c29dd207c24210b7
Fill <zubareff10@gmail.com>
2020-03-02 02:46:28 +0500

    Задача: "Тренировки в полёте" и "Список профессий" и "Автоматический ответ" и "Двойная защита".

--- cgi-bin/lesson.py
+++ cgi-bin/lesson.py
@@ -1,4 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    user_id = StringField('id Астронавта', validators=[DataRequired()])
+    user_pass = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    captain_id = StringField('id капитана', validators=[DataRequired()])
+    captain_pas = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -6,7 +18,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 @app.route('/')
 @app.route('/index')
-def login():
+def index():
     return render_template('base.html', title='Авторизация')
 
 
@@ -45,6 +57,15 @@ def answer():
     send_vals = list(map(lambda x: x, data.values()))
     return render_template('auto_answer.html', link=url_for('static', filename='css/mars_style.css'), values=send_vals,
                            size=len(data.keys()))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        print(1)
+    return render_template('double_login.html', title='Авторизация', form=form,
+                           link=url_for('static', filename='css/mars_style.css'))
 
 
 if __name__ == '__main__':
